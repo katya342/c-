5695da99bdbcbf422875ffc68a5aecb5e6dea10f
Екатерина Безверхова <katya.bezverkhova@mail.ru>
2022-08-29 14:44:34 +0600

    dz

---
+++ dz_1/dz_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DZ_1();
+        }
+        static void DZ_1()
+        {
+            Console.WriteLine("Введите ФИО: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Приветствую тебя - " + name);
+            int sum = 0;
+            for(int i = 0; i < 5; i++)
+            {
+                Console.Write("Введите число: ");
+                int numbers = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Enter numbers: " + numbers);
+                sum = sum + numbers;
+            }
+            Console.WriteLine("Summa: " + sum);
+        }
+    }
+}
