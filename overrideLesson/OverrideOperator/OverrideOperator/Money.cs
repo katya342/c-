5de4772a4990bc92fbf7bada29ec62e1dd35ee5b
Екатерина Безверхова <katya.bezverkhova@mail.ru>
2022-09-14 20:33:10 +0600

    overrideOperators

---
+++ overrideLesson/OverrideOperator/OverrideOperator/Money.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverrideOperator
+{
+    public class Number
+    {
+        public decimal Amount { get; set; }
+        public string Unit { get; set; }
+        public int SomeValue { get; set; }
+        public static explicit operator Number(Money m)
+        {
+            return new Number { Amount = m.Amount };
+        }
+    }
+    public class Money
+    {
+        public decimal Amount { get; set; }
+        public string Unit { get; set; }
+
+        public Money(decimal Amount, string Unit)
+        {
+            this.Amount = Amount;
+            this.Unit = Unit;
+        }
+        public static Money operator +(Money a, Money b)
+        {
+            if (a.Unit != b.Unit)
+                throw new InvalidOperationException("Нельзя");
+            return new Money(a.Amount + b.Amount, a.Unit);
+        }
+        public override string ToString()
+        {
+            return String.Format("{0} - {1}", this.Amount, this.Unit);
+        }
+        public override bool Equals(object obj)
+        {
+            return ((Money)obj).Amount == this.Amount;
+        }
+
+        public static bool operator ==(Money a, Money b)
+        {
+            return a.Amount == b.Amount;
+        }
+        public static bool operator !=(Money a, Money b)
+        {
+            return a.Amount != b.Amount;
+        }
+
+    }
+}
---
+++ overrideLesson/OverrideOperator/OverrideOperator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverrideOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Money money_1 = new Money(100, "USD");
+            Money money_2 = new Money(300, "USD");
+            Console.WriteLine(money_1);
+            Money money_3 = money_1 + money_2;
+            Number num = (Number)money_1;
+            var data = "";
+        }
+    }
+}
