c76990fb91fae6eaf229e2a7ad7e4777d6b2c4ac
Екатерина Безверхова <katya.bezverkhova@mail.ru>
2022-10-19 16:23:12 +0600

    index

---
+++ indexes_dz/indexes_dz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace indexes_dz
+{
+    
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var tempRecord = new TempRecord();
+
+            // Use the indexer's set accessor
+            tempRecord[3] = 58.3F;
+            tempRecord[5] = 60.1F;
+
+            // Use the indexer's get accessor
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Element #{i} = {tempRecord[i]}");
+            }
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
